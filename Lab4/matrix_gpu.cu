1e6b7f49fb748c6abf4524879d667ba87d799f8c
Adam <adamsdm1@gmail.com>
2017-11-28 15:23:32 +0100

    Added lab4

---
+++ Lab4/matrix_gpu.cu
@@ -0,0 +1,126 @@
+
+#include "cuda_runtime.h"
+#include "device_launch_parameters.h"
+
+#include <stdio.h>
+
+
+const int N = 1024;
+const int blocksize = 3;
+
+__global__ void add_matrix(float *a, float *b, float *c) {
+	
+	int index;
+	int row = blockIdx.y * blockDim.y + threadIdx.y;
+	int col = blockIdx.x * blockDim.x + threadIdx.x;
+
+
+	if (row < N && col < N)
+		c[row + col*N] = a[row + col*N] + b[row + col*N];
+
+}
+
+int main()
+{
+	float gpuTime = 0;
+	float cpuTime = 0;
+
+	float *a = new float[N*N];
+	float *b = new float[N*N];
+	float *c = new float[N*N];
+
+	float *d_a, *d_b, *d_c;
+
+	// Allocate memory on device
+	cudaMalloc((void**)&d_a, N * N * sizeof(float));
+	cudaMalloc((void**)&d_b, N * N * sizeof(float));
+	cudaMalloc((void**)&d_c, N * N * sizeof(float));
+
+	// Initialize matrices
+	for (int i = 0; i < N; i++){
+		for (int j = 0; j < N; j++){
+			a[i + j*N] = i;
+			b[i + j*N] = 2*i;
+		}
+	}
+
+	cudaMemcpy(d_a, a, N * N * sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_b, b, N * N * sizeof(float), cudaMemcpyHostToDevice);
+	cudaMemcpy(d_c, c, N * N * sizeof(float), cudaMemcpyHostToDevice);
+	
+	// Optimal solution
+	dim3 threadsPerBlock(16, 16);
+	dim3 numBlocks(N / threadsPerBlock.x, N / threadsPerBlock.y);
+	
+	//dim3 threadsPerBlock(N, N);
+	//dim3 numBlocks(1, 1);
+
+	// Timing
+	cudaEvent_t start, stop;
+	cudaEventCreate(&start);
+	cudaEventCreate(&stop);
+
+	// Execute the kernel
+	cudaEventRecord(start);
+	add_matrix <<<numBlocks, threadsPerBlock >>> (d_a, d_b, d_c);
+	cudaThreadSynchronize();
+	cudaEventRecord(stop);
+
+	cudaEventSynchronize(stop);
+	cudaEventElapsedTime(&gpuTime, start, stop);
+	
+
+	// sync threads
+	
+	cudaMemcpy(c, d_c, N * N * sizeof(float), cudaMemcpyDeviceToHost);
+	
+	cudaFree(d_a);
+	cudaFree(d_b);
+	cudaFree(d_c);
+	
+	// Asert result is correct
+	for (int i = 0; i < N; i++) {
+		for (int j = 0; j < N; j++) {
+			int index = i + j*N;
+
+			if (c[index] != a[index] + b[index]) {
+				printf("Error: %f + %f != %f\n", a[index], b[index], c[index]);
+				return;
+			}
+		}
+
+		/*
+		// A
+		printf("|");
+		for (int j = 0; j < N; j++) {
+			printf("%.2f ", a[i + j*N]);
+		}
+		printf("|\t");
+
+		// B
+		printf("|");
+		for (int j = 0; j < N; j++) {
+			printf("%.2f ", b[i + j*N]);
+		}
+		printf("|\t");
+
+		// C
+		printf("|");
+		for (int j = 0; j < N; j++) {
+			printf("%.2f ", c[i + j*N]);
+		}
+		printf("|\t\n");
+		*/
+	}
+
+	printf("Problem size: %i\t\n", N);
+	printf("GPU Time: \t%f\n", gpuTime);
+	printf("CPU Time: \t%f\n", cpuTime);
+
+
+	delete[] a;
+	delete[] b;
+	delete[] c;
+	
+    return 0;
+}
