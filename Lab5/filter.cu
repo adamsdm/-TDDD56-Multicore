5f1a99b08e38bbdbed925105b83853f94082f9fd
Adam Söderström <adaso578@southfork-08.edu.isy.liu.se>
2017-12-13 16:18:50 +0100

    Lab5 done

--- Lab5/filter.cu
+++ Lab5/filter.cu
@@ -38,8 +38,8 @@
 #define maxKernelSizeY 20
 
 #define SUB_SIZE 16
-#define FILTER_RAD_X 8
-#define FILTER_RAD_Y 8
+#define FILTER_RAD_X 10
+#define FILTER_RAD_Y 10
 
 
 __device__ const unsigned int CACHE_SIZE_X = (2 * maxKernelSizeX + 1) * 3; // RGB
@@ -65,6 +65,38 @@ __device__ void addPadding(unsigned char *image, int kernelsizex, int kernelsize
   }
 }
 
+__global__ void naive_filter(unsigned char *image, unsigned char *out, const unsigned int imagesizex, const unsigned int imagesizey, const int kernelsizex, const int kernelsizey)
+{
+  // map from blockIdx to pixel position
+	int x = blockIdx.x * blockDim.x + threadIdx.x;
+	int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+  int dy, dx;
+  unsigned int sumx, sumy, sumz;
+
+  int divby = (2*kernelsizex+1)*(2*kernelsizey+1); // Works for box filters only!
+
+	if (x < imagesizex && y < imagesizey) // If inside image
+	{
+// Filter kernel (simple box filter)
+	sumx=0;sumy=0;sumz=0;
+	for(dy=-kernelsizey;dy<=kernelsizey;dy++)
+		for(dx=-kernelsizex;dx<=kernelsizex;dx++)
+		{
+			// Use max and min to avoid branching!
+			int yy = min(max(y+dy, 0), imagesizey-1);
+			int xx = min(max(x+dx, 0), imagesizex-1);
+
+			sumx += image[((yy)*imagesizex+(xx))*3+0];
+			sumy += image[((yy)*imagesizex+(xx))*3+1];
+			sumz += image[((yy)*imagesizex+(xx))*3+2];
+		}
+	out[(y*imagesizex+x)*3+0] = sumx/divby;
+	out[(y*imagesizex+x)*3+1] = sumy/divby;
+	out[(y*imagesizex+x)*3+2] = sumz/divby;
+	}
+}
+
 __global__ void filter(unsigned char *image, unsigned char *out, const unsigned int imagesizey, const unsigned int imagesizex, const int kernelsizex, const int kernelsizey)
 {
   // map from blockIdx to pixel position
@@ -84,6 +116,7 @@ __global__ void filter(unsigned char *image, unsigned char *out, const unsigned 
   __syncthreads();
 
 	// Print shared memory
+  /*
 	if (blockIdx.y == 0 && blockIdx.x == 7 && threadIdx.x == 0 && threadIdx.y == 0) {
 		printf("\n");
 
@@ -100,6 +133,7 @@ __global__ void filter(unsigned char *image, unsigned char *out, const unsigned 
 
 		printf("\n\n");
 	}
+  */
 
 
 	unsigned int divby = (2 * kernelsizex + 1)*(2 * kernelsizey + 1);
@@ -111,6 +145,7 @@ __global__ void filter(unsigned char *image, unsigned char *out, const unsigned 
 
 
   // Print averaging for pixel 0
+  /*
   if (blockIdx.y == 0 && blockIdx.x == 7 && threadIdx.x == 0 && threadIdx.y == 0) {
     for (int dy = -kernelsizey; dy <= kernelsizey; dy++) {
       for (int dx = -kernelsizex; dx <= kernelsizex; dx++) {
@@ -124,6 +159,7 @@ __global__ void filter(unsigned char *image, unsigned char *out, const unsigned 
       printf("\n");
     }
   }
+  */
 
 
 	//if (localX < blockDim.x + kernelsizex && localY < blockDim.y + kernelsizey &&
@@ -148,9 +184,129 @@ __global__ void filter(unsigned char *image, unsigned char *out, const unsigned 
 
 }
 
+__global__ void gaussian_filter(unsigned char *image, unsigned char *out, const unsigned int imagesizey, const unsigned int imagesizex, const int kernelsizex, const int kernelsizey)
+{
+  // map from blockIdx to pixel position
+	int x = blockIdx.x * blockDim.x + threadIdx.x;
+	int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+
+	if (x > imagesizex) return;
+	if (y > imagesizey) return;
+
+
+  // Single thread padding
+  if(threadIdx.x == 0 && threadIdx.y == 0){
+    addPadding(image, kernelsizex, kernelsizey, imagesizex, imagesizey);
+  }
+
+  __syncthreads();
+
+
+	unsigned int sumx, sumy, sumz;
+	int localY = threadIdx.y + kernelsizey;
+	int localX = threadIdx.x + kernelsizex;
+	sumx = 0; sumy = 0; sumz = 0;
+
+  const int gaussWeight[] = {1, 4, 6, 4, 1};
+  unsigned int divby = 16;
+
+	for (int dy = -kernelsizey; dy <= kernelsizey; dy++) {
+		for (int dx = -kernelsizex; dx <= kernelsizex; dx++) {
+
+			int yy = localY + dy;
+			int xx = localX + dx;
+
+      int index = (dx + kernelsizex) + (dy + kernelsizey);
+
+			sumx += cacheShared[xx * 3 + 0][yy] * gaussWeight[index];
+			sumy += cacheShared[xx * 3 + 1][yy] * gaussWeight[index];
+			sumz += cacheShared[xx * 3 + 2][yy] * gaussWeight[index];
+		}
+	}
+
+	out[(y*imagesizex + x) * 3 + 0] = sumx / divby;
+	out[(y*imagesizex + x) * 3 + 1] = sumy / divby;
+	out[(y*imagesizex + x) * 3 + 2] = sumz / divby;
+
+}
+
+
+__device__ int find_median(int arr[], int N){
+
+  int i = 1;
+  int j, tmp;
+  while( i < N ){
+    j=i;
+    while(j>0 && arr[j-1] > arr[j]){
+        tmp = arr[j];
+        arr[j] = arr[j-1];
+        arr[j-1] = tmp;
+
+        j--;
+    }
+    i++;
+  }
+
+  return arr[N/2];
+}
+
+__global__ void median_filter(unsigned char *image, unsigned char *out, const unsigned int imagesizey, const unsigned int imagesizex, const int kernelsizex, const int kernelsizey)
+{
+  // map from blockIdx to pixel position
+	int x = blockIdx.x * blockDim.x + threadIdx.x;
+	int y = blockIdx.y * blockDim.y + threadIdx.y;
+
+
+	if (x > imagesizex) return;
+	if (y > imagesizey) return;
+
+
+  // Single thread padding
+  if(threadIdx.x == 0 && threadIdx.y == 0){
+    addPadding(image, kernelsizex, kernelsizey, imagesizex, imagesizey);
+  }
+
+  __syncthreads();
+
+
+	unsigned int sumx, sumy, sumz;
+	int localY = threadIdx.y + kernelsizey;
+	int localX = threadIdx.x + kernelsizex;
+	sumx = 0; sumy = 0; sumz = 0;
+
+  int kernel_arrR[(maxKernelSizeX*2+1) + (maxKernelSizeY*2+1)];
+  int kernel_arrG[(maxKernelSizeX*2+1) + (maxKernelSizeY*2+1)];
+  int kernel_arrB[(maxKernelSizeX*2+1) + (maxKernelSizeY*2+1)];
+
+	for (int dy = -kernelsizey; dy <= kernelsizey; dy++) {
+		for (int dx = -kernelsizex; dx <= kernelsizex; dx++) {
+
+			int yy = localY + dy;
+			int xx = localX + dx;
+
+      int index = (dx + kernelsizex) + (dy + kernelsizey);
+
+			kernel_arrR[index] = cacheShared[xx * 3 + 0][yy];
+			kernel_arrG[index] = cacheShared[xx * 3 + 1][yy];
+			kernel_arrB[index] = cacheShared[xx * 3 + 2][yy];
+		}
+	}
+
+  int medR = find_median(kernel_arrR, kernelsizex + kernelsizey);
+  int medG = find_median(kernel_arrG, kernelsizex + kernelsizey);
+  int medB = find_median(kernel_arrB, kernelsizex + kernelsizey);
+
+
+	out[(y*imagesizex + x) * 3 + 0] = medR;
+	out[(y*imagesizex + x) * 3 + 1] = medG;
+	out[(y*imagesizex + x) * 3 + 2] = medB;
+
+}
+
 // Global variables for image data
 
-unsigned char *image, *pixels, *dev_bitmap, *dev_input;
+unsigned char *image, *pixels, *dev_bitmap, *dev_temp_output, *dev_input;
 unsigned int imagesizey, imagesizex; // Image size
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -168,12 +324,52 @@ void computeImages(int kernelsizex, int kernelsizey)
 	cudaMalloc( (void**)&dev_input, imagesizex*imagesizey*3);
 	cudaMemcpy( dev_input, image, imagesizey*imagesizex*3, cudaMemcpyHostToDevice );
 	cudaMalloc( (void**)&dev_bitmap, imagesizex*imagesizey*3);
+  cudaMalloc( (void**)&dev_temp_output, imagesizex*imagesizey*3);
 
   dim3 grid(imagesizex/SUB_SIZE,imagesizey/SUB_SIZE);
   dim3 block(SUB_SIZE, SUB_SIZE);
+  printf("\n");
 
+  // Naive version
+
+  ResetMilli();
+	naive_filter<<<grid,block>>>(dev_input, dev_bitmap, imagesizey, imagesizex, kernelsizex, kernelsizey); // Awful load balance
+  printf("Naive: \t\t\t%f\n", GetSeconds());
+  cudaThreadSynchronize();
+
+
+  /*
+  // Task 1
+  ResetMilli();
 	filter<<<grid,block>>>(dev_input, dev_bitmap, imagesizey, imagesizex, kernelsizex, kernelsizey); // Awful load balance
-	cudaThreadSynchronize();
+  printf("Averaging: \t\t%f\n", GetSeconds());
+  cudaThreadSynchronize();
+
+
+  // Task 2 - seperable filter kernels
+  ResetMilli();
+  filter<<<grid,block>>>(dev_input, dev_temp_output, imagesizey, imagesizex, 0, kernelsizey);
+  filter<<<grid,block>>>(dev_temp_output, dev_bitmap, imagesizey, imagesizex, kernelsizex, 0);
+  printf("Avg. Seperable: \t%f\n", GetSeconds());
+  cudaThreadSynchronize();
+
+  // Task 3 - seperable gaussian
+  ResetMilli();
+  gaussian_filter<<<grid,block>>>(dev_input, dev_temp_output, imagesizey, imagesizex, 0, 2);
+  gaussian_filter<<<grid,block>>>(dev_temp_output, dev_bitmap, imagesizey, imagesizex, 2, 0);
+  printf("Gauss. Seperable: \t%f\n", GetSeconds());
+  cudaThreadSynchronize();
+  */
+  // Task 4 - Median
+  ResetMilli();
+  median_filter<<<grid,block>>>(dev_input, dev_temp_output, imagesizey, imagesizex, 0, FILTER_RAD_Y);
+  median_filter<<<grid,block>>>(dev_temp_output, dev_bitmap, imagesizey, imagesizex, FILTER_RAD_X, 0);
+  printf("Medi. Seperable: \t%f\n", GetSeconds());
+  cudaThreadSynchronize();
+
+
+
+
 //	Check for errors!
     cudaError_t err = cudaGetLastError();
     if (err != cudaSuccess)
@@ -216,7 +412,7 @@ int main( int argc, char** argv)
 	if (argc > 1)
 		image = readppm(argv[1], (int *)&imagesizex, (int *)&imagesizey);
 	else
-		image = readppm((char *)"maskros512.ppm", (int *)&imagesizex, (int *)&imagesizey);
+		image = readppm((char *)"maskros-noisy.ppm", (int *)&imagesizex, (int *)&imagesizey);
 
 	if (imagesizey >= imagesizex)
 		glutInitWindowSize( imagesizex*2, imagesizey );
